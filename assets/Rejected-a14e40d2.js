4a174e697dcd87716cc352cf26e7777cdf0eeca6
Naveen Jayathilaka <tharindunaveen9@gmail.com>
2024-03-14 16:10:13 +0530

    Updates

--- assets/Rejected-a14e40d2.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./react-fbb0bda4.js";import"./@babel-ee410cf9.js";const o=()=>e.jsx("div",{children:"Rejected"});export{o as default};
--- assets/Verified-39174e3d.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./react-fbb0bda4.js";import"./@babel-ee410cf9.js";const t=()=>r.jsx("div",{children:"Verified"});export{t as default};
