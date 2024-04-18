aa606fa0022871edfa7ad2fa3223248841d8e34e
Naveen Jayathilaka <tharindunaveen9@gmail.com>
2024-04-18 19:38:00 +0530

    drawer mobile resposive

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -6,12 +6,12 @@ import Drawer from "@mui/material/Drawer";
 import CssBaseline from "@mui/material/CssBaseline";
 import MuiAppBar from "@mui/material/AppBar";
 import { useLocation, useNavigate } from "react-router-dom";
-
+import useResponsive from "../hooks/useResponsive";
 import Navigation from "./Navigation";
 import Header from "./Header";
 import { Divider } from "@mui/material";
 
-const drawerWidth = 230;
+const drawerWidth = 200;
 
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
@@ -21,7 +21,7 @@ const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    marginLeft: `-${drawerWidth}px`,
+    marginLeft: useResponsive("up", "lg") ? `-${drawerWidth}px` : 0,
     ...(open && {
       transition: theme.transitions.create("margin", {
         easing: theme.transitions.easing.easeOut,
@@ -40,7 +40,7 @@ const AppBar = styled(MuiAppBar, {
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
+    width: useResponsive("up", "lg") ? `calc(100% - ${drawerWidth}px)` : "100%",
     marginLeft: `${drawerWidth}px`,
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
@@ -59,8 +59,7 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 }));
 
 function Layout() {
-  const theme = useTheme();
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(useResponsive("up", "lg"));
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -104,21 +103,34 @@ function Layout() {
         <Divider />
       </AppBar>
       <Drawer
+        slotProps={{
+          backdrop: {
+            sx: {
+              background: "rgba(0,0,0,0.4)",
+            },
+          },
+        }}
         sx={{
           width: drawerWidth,
-          flexShrink: 0,
+          // flexShrink: 0,
           "& .MuiDrawer-paper": {
             width: drawerWidth,
             background: "transparent",
             border: "none",
+            borderRadius: 2,
+            boxShadow: 3,
           },
         }}
-        variant="persistent"
+        variant={useResponsive("up", "lg") ? "persistent" : "temporary"}
         anchor="left"
         open={open}
+        onClose={handleDrawerClose}
       >
-        <Box height={"100%"} pr={1}>
-          <Navigation currentUrl={currentUrl} />
+        <Box height={"100%"}>
+          <Navigation
+            currentUrl={currentUrl}
+            handleDrawerClose={handleDrawerClose}
+          />
         </Box>
       </Drawer>
       <Main open={open}>
--- src/pages/Dashboard/Overview.jsx
+++ src/pages/Dashboard/Overview.jsx
@@ -236,41 +236,111 @@ const Overview = () => {
     <Grid container spacing={2} width={"100%"}>
       {/* --------------------------Filter Options---------------------------------- */}
 
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <DateFilter date={date} setDate={setDate} today={today} />
       </Grid>
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <CategoryFilter category={category} setCategory={setCategory} />
       </Grid>
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <SubCategoryFilter
           subCategory={subCategory}
           setSubCategory={setSubCategory}
         />
       </Grid>
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <StatusFilter status={status} setStatus={setStatus} />
       </Grid>
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <ProvinceFilter province={province} setProvince={setProvince} />
       </Grid>
       <Grid item xs={12}></Grid>
 
       {/* ---------------------------Count Cards-------------------------------------- */}
 
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <CountCard count={counts.totalSupplier} title={"Total suppliers"} />
       </Grid>
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <CountCard count={counts.product} title={"Products"} />
       </Grid>
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <CountCard count={counts.rawMaterial} title={"Raw materials"} />
       </Grid>
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <CountCard count={counts.joinCount} title={"Join with CEHub"} />
       </Grid>
-      <Grid item xs={2.4} display={"flex"} justifyContent={"center"}>
+      <Grid
+        item
+        lg={2.4}
+        md={4}
+        xs={6}
+        display={"flex"}
+        justifyContent={"center"}
+      >
         <CountCard count={counts.joinPercentage} title={"Join with CEHub %"} />
       </Grid>
       <Grid item xs={12}></Grid>
