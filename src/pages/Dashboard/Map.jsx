61b897e50e3e9a6a4a65f89696e5253185caa1f4
Naveen Jayathilaka <tharindunaveen9@gmail.com>
2024-05-09 16:46:33 +0530

    map updated

--- src/pages/Dashboard/Map.jsx
+++ src/pages/Dashboard/Map.jsx
@@ -6,9 +6,22 @@ import { CategoryFilter } from "../../components/FilterComponents/CategoryFilter
 import { SubCategoryFilter } from "../../components/FilterComponents/SubCategoryFilter";
 import { StatusFilter } from "../../components/FilterComponents/StatusFilter";
 import { ProvinceFilter } from "../../components/FilterComponents/ProvinceFilter";
-import GoogleMapReact from "google-map-react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
+const center = {
+  lat: 7.2905715, // default latitude
+  lng: 80.6337262, // default longitude
+};
 
 const Map = () => {
   const today = dayjs().format("YYYY-MM-DD");
@@ -19,13 +32,51 @@ const Map = () => {
   const [status, setStatus] = useState("All");
   const [province, setProvince] = useState("All");
 
-  const defaultProps = {
-    center: {
-      lat: 7.8774222,
-      lng: 80.7003428,
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyAaR7EXzdD_ECrIwHSQGT2fIidx8-26m2M",
+    libraries,
+  });
+
+  const [locations, setLocations] = useState(
+    [{
+      lat: 7.2905715,
+      lng: 80.6337262,
+      id: "1",
+      number: "0769114074",
+      name: "Naveen Jayathilaka",
     },
-    zoom: 9,
-  };
+    {
+      lat: 7.2905715,
+      lng: 81.6337262,
+      id: "2",
+      number: "0769114074",
+      name: "Naveen Jayathilaka",
+    },
+    {
+      lat: 8.2905715,
+      lng: 80.6337262,
+      id: "3",
+      number: "0769114074",
+      name: "Naveen Jayathilaka",
+    },
+    {
+      lat: 6.2905715,
+      lng: 80.1337262,
+      id: "4",
+      number: "0769114074",
+      name: "Naveen Jayathilaka",
+    },
+    {
+      lat: 6.6905715,
+      lng: 80.1337262,
+      id: "5",
+      number: "0769114074",
+      name: "Naveen Jayathilaka",
+    },]
+  );
+
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
   return (
     <Grid container spacing={2} width={"100%"}>
       {/* --------------------------Filter Options---------------------------------- */}
@@ -85,20 +136,41 @@ const Map = () => {
       </Grid>
 
       {/* --------------Map---------- */}
-      
+
       <Grid item xs={12}>
-        <Box height={"90svh"} width={"100%"}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: "" }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
-          >
-            <AnyReactComponent
-              lat={59.955413}
-              lng={30.337844}
-              text="My Marker"
-            />
-          </GoogleMapReact>
+        <Box height={"80svh"} width={"100%"}>
+          {isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              zoom={10}
+              center={center}
+            >
+              {locations.map((location, index) => (
+                <Marker
+                  key={index}
+                  position={{ lat: location.lat, lng: location.lng }}
+                  onClick={() => setSelectedLocation(location)}
+                />
+              ))}
+
+              {selectedLocation && (
+                <InfoWindow
+                  position={{
+                    lat: selectedLocation.lat,
+                    lng: selectedLocation.lng,
+                  }}
+                  onCloseClick={() => setSelectedLocation(null)}
+                >
+                  <div>
+                    <h3>{selectedLocation.name}</h3>
+                    <p>Phone: {selectedLocation.number}</p>
+                  </div>
+                </InfoWindow>
+              )}
+            </GoogleMap>
+          ) : (
+            <></>
+          )}
         </Box>
       </Grid>
     </Grid>
